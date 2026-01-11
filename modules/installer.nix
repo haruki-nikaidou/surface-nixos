80364f869c2258f0f6b4fcdea58f2457bc6132ea
Nikaidou Haruki <nharuki@pm.me>
2026-01-11 11:37:47 +0900

    use plasma 6 as base

--- modules/installer.nix
+++ modules/installer.nix
@@ -8,28 +8,11 @@
 
 {
   imports = [
-    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+    "${modulesPath}/installer/cd-dvd/installation-cd-graphical-calamares-plasma6.nix"
   ];
 
   # Disable ZFS - it's broken on kernel 6.18+
   boot.supportedFilesystems.zfs = lib.mkForce false;
-
-  # ISO naming
-  image.fileName = lib.mkForce "nixos-surface-laptop7-${config.system.nixos.label}-aarch64.iso";
-  isoImage.volumeID = lib.mkForce "NIXOS_SL7";
-
-  # Use GRUB for EFI boot (required for DTB loading)
-  isoImage.makeEfiBootable = true;
-  isoImage.makeUsbBootable = true;
-
-  # GRUB configuration
-  boot.loader.grub = {
-    enable = lib.mkForce true;
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-    device = "nodev";
-    memtest86.enable = lib.mkForce false; # Not available on aarch64
-  };
 
   # Add DTBs to ISO and configure boot
   isoImage.contents = [
@@ -38,9 +21,6 @@
       target = "/dtbs";
     }
   ];
-
-  # Squash some warnings
-  isoImage.squashfsCompression = "zstd -Xcompression-level 6";
 
   # GRUB config to include DTB loading
   # This creates additional boot entries with devicetree
@@ -66,69 +46,6 @@
 
   # Add Surface-specific kernel params
   boot.kernelParams = [ "cma=128M" ];
-
-  # Alternative: Use extraEntries for DTB-loading menu entries
-  # Note: The initrd handles finding init automatically on NixOS ISOs
-  boot.loader.grub.extraEntries =
-    let
-      kernelFile = config.system.boot.loader.kernelFile;
-      initrdFile = config.system.boot.loader.initrdFile;
-      volumeID = config.isoImage.volumeID;
-      # Hardcode essential boot params to avoid circular dependency with config.boot.kernelParams
-      bootParams = "findiso= root=live:LABEL=${volumeID} rd.live.image cma=128M";
-    in
-    ''
-      menuentry "NixOS Installer - Surface Laptop 7" --class nixos {
-        terminal_output gfxterm
-        search --set=root --label ${volumeID}
-        linux /boot/${kernelFile} ${bootParams}
-        initrd /boot/${initrdFile}
-        devicetree /dtbs/qcom/x1p64100-microsoft-denali.dtb
-      }
-      menuentry "NixOS Installer - Debug (break=top)" --class nixos {
-        terminal_output gfxterm
-        search --set=root --label ${volumeID}
-        linux /boot/${kernelFile} ${bootParams} break=top
-        initrd /boot/${initrdFile}
-        devicetree /dtbs/qcom/x1p64100-microsoft-denali.dtb
-      }
-    '';
-
-  # Enable SSH for headless install (useful if display doesn't work)
-  systemd.services.sshd.wantedBy = lib.mkForce [ "multi-user.target" ];
-  services.openssh = {
-    enable = true;
-    settings.PermitRootLogin = "yes";
-  };
-  users.users.root = {
-    initialPassword = "nixos";
-    initialHashedPassword = lib.mkForce null;
-  };
-  users.users.nixos = {
-    initialPassword = "nixos";
-    initialHashedPassword = lib.mkForce null;
-  };
-
-  # Networking for installation
-  networking = {
-    hostName = "nixos-installer";
-    wireless.enable = true; # We'll use NetworkManager
-    networkmanager.enable = true;
-  };
-
-  # XFCE Desktop Environment for GUI installation
-  services.xserver = {
-    enable = true;
-    desktopManager.xfce.enable = true;
-    displayManager.lightdm = {
-      enable = true;
-      greeters.slick.enable = true;
-    };
-  };
-  services.displayManager.autoLogin = {
-    enable = true;
-    user = "nixos";
-  };
 
   # Useful packages in installer
   environment.systemPackages = with pkgs; [
@@ -162,10 +79,4 @@
     # Note: fwupd is excluded - it doesn't cross-compile (fwupd-efi needs native ld.bfd)
     # Install it after booting the native system
   ];
-
-  # Console font (readable on HiDPI)
-  console = {
-    font = "ter-v32n";
-    packages = [ pkgs.terminus_font ];
-  };
 }
